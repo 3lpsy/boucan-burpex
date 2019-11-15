15fdffd08c17530695b3f3169f1c8a988cc79649
3lpsy <elpsy@protonmail.com>
2019-11-14 19:23:27 -0600

    update

--- BoucanBurpex.py
+++ BoucanBurpex.py
@@ -21,11 +21,14 @@ except ImportError:
 callbacks = None
 helpers = None
 
-class FakeSocket():
+
+class FakeSocket:
     def __init__(self, response_str):
         self._file = StringIO(response_str)
+
     def makefile(self, *args, **kwargs):
         return self._file
+
 
 class PyRunnable(Runnable):
     def __init__(self, target, *args, **kwargs):
@@ -36,13 +39,14 @@ class PyRunnable(Runnable):
     def run(self):
         self.target(*self.args, **self.kwargs)
 
+
 class ApiClient(object):
     def __init__(self, api_url, api_token):
         self._api_url = api_url
         self._api_token = api_token
 
     def get_zones(self):
-        url = urlparse(self.url('/zone'))
+        url = urlparse(self.url("/zone"))
         # print "Building request"
         request = self.build_request("get", url, self._api_token)
         # print "Hostname: " + str(self.hostname())
@@ -52,7 +56,7 @@ class ApiClient(object):
         try:
             response = callbacks.makeHttpRequest(self.service(), request)
             status = int(response.getStatusCode())
-            return self.get_json(response)['zones']
+            return self.get_json(response)["zones"]
 
         except:
             print "HTTP Error. Could not get Zones"
@@ -67,14 +71,16 @@ class ApiClient(object):
         requestString.write("HOST: ")
         requestString.write(str(url.hostname))
         requestString.write("\r\n")
-        requestString.write("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.28) Gecko/00000000 Firefox/30.0.00\r\n")
-        requestString.write('Accept: application/json\r\n')
+        requestString.write(
+            "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.28) Gecko/00000000 Firefox/30.0.00\r\n"
+        )
+        requestString.write("Accept: application/json\r\n")
         if token:
-            requestString.write('Authorization: Bearer ')
+            requestString.write("Authorization: Bearer ")
             requestString.write(str(token))
             requestString.write("\r\n")
 
-        requestString.write('\r\n\r\n')
+        requestString.write("\r\n\r\n")
         # print "Request: " + str(requestString.getvalue())
         Request = map(lambda x: ord(x), requestString.getvalue())
         requestString.close()
@@ -95,12 +101,13 @@ class ApiClient(object):
         port = self.parsed_url().port
         if port:
             return int(port)
-        elif self.parsed_url().scheme == 'https':
+        elif self.parsed_url().scheme == "https":
             return 443
         else:
             return 80
+
     def is_https(self):
-        return self.parsed_url().scheme == 'https'
+        return self.parsed_url().scheme == "https"
 
     def hostname(self):
         hostname = self.parsed_url().hostname
@@ -113,7 +120,10 @@ class ApiClient(object):
         return urlparse(self._api_url)
 
     def service(self):
-        return helpers.buildHttpService(str(self.hostname()), self.port(), self.is_https())
+        return helpers.buildHttpService(
+            str(self.hostname()), self.port(), self.is_https()
+        )
+
 
 class BurpExtender(IBurpExtender, ITab):
     def registerExtenderCallbacks(self, _callbacks):
@@ -124,7 +134,7 @@ class BurpExtender(IBurpExtender, ITab):
         helpers = callbacks.getHelpers()
         self._helpers = helpers
         sys.stdout = callbacks.getStdout()
-        self._callbacks.setExtensionName("BountyDNS")
+        self._callbacks.setExtensionName("Boucan")
         self._panel = self.buildPanel()
         self.loadZones()
 
@@ -133,67 +143,69 @@ class BurpExtender(IBurpExtender, ITab):
         return
 
     def buildPanel(self):
-        print 'Building Panel...'
+        print "Building Panel..."
         panel = swing.JPanel()
         boxVertical = swing.Box.createVerticalBox()
 
-        print 'Creating API URL Label...'
+        print "Creating API URL Label..."
         boxHorizontal = swing.Box.createHorizontalBox()
         boxHorizontal.add(swing.JLabel("Enter API URL"))
         boxVertical.add(boxHorizontal)
 
-        print 'Creating API URL Text Field...'
+        print "Creating API URL Text Field..."
         boxHorizontal = swing.Box.createHorizontalBox()
         self._apiUrl = swing.JTextField(self.loadApiUrl(), 30)
         boxHorizontal.add(self._apiUrl)
         boxVertical.add(boxHorizontal)
 
-        print 'Creating API Token Label...'
+        print "Creating API Token Label..."
         boxHorizontal = swing.Box.createHorizontalBox()
         boxHorizontal.add(swing.JLabel("Enter API Token"))
         boxVertical.add(boxHorizontal)
 
-        print 'Creating API Token Text Field...'
+        print "Creating API Token Text Field..."
         boxHorizontal = swing.Box.createHorizontalBox()
         self._apiToken = swing.JTextArea()
         apiTokenOuput = swing.JScrollPane(self._apiToken)
-        apiTokenOuput.setPreferredSize(Dimension(250,125))
+        apiTokenOuput.setPreferredSize(Dimension(250, 125))
         self._apiToken.setText(self.loadApiToken())
         self._apiTokenOuput = apiTokenOuput
         boxHorizontal.add(self._apiTokenOuput)
         boxVertical.add(boxHorizontal)
 
-        print 'Adding Update Button'
+        print "Adding Update Button"
         boxHorizontal = swing.Box.createHorizontalBox()
-        updateConfButton = swing.JButton('Update Configuration', actionPerformed=self.updateConfEvent)
+        updateConfButton = swing.JButton(
+            "Update Configuration", actionPerformed=self.updateConfEvent
+        )
         boxHorizontal.add(updateConfButton)
         boxVertical.add(boxHorizontal)
 
         self._zones = []
-        print 'Creating Zones Output Box'
+        print "Creating Zones Output Box"
         boxHorizontal = swing.Box.createHorizontalBox()
         boxHorizontal.add(swing.JLabel("Loaded Zones (Output)"))
         boxVertical.add(boxHorizontal)
         boxHorizontal = swing.Box.createHorizontalBox()
         self._zonesLoaded = swing.JTextArea()
         zonesLoaded = swing.JScrollPane(self._zonesLoaded)
-        zonesLoaded.setPreferredSize(Dimension(250,125))
+        zonesLoaded.setPreferredSize(Dimension(250, 125))
         boxHorizontal.add(zonesLoaded)
         boxVertical.add(boxHorizontal)
 
         self._payloads = []
-        print 'Creating Payloads Output Box'
+        print "Creating Payloads Output Box"
         boxHorizontal = swing.Box.createHorizontalBox()
         boxHorizontal.add(swing.JLabel("Submitted Payloads (Output)"))
         boxVertical.add(boxHorizontal)
         boxHorizontal = swing.Box.createHorizontalBox()
         self._payloadsSubmitted = swing.JTextArea()
         payloadsSubmitted = swing.JScrollPane(self._payloadsSubmitted)
-        payloadsSubmitted.setPreferredSize(Dimension(250,125))
+        payloadsSubmitted.setPreferredSize(Dimension(250, 125))
         boxHorizontal.add(payloadsSubmitted)
         boxVertical.add(boxHorizontal)
 
-        print 'Adding Vertical Box to Panel...'
+        print "Adding Vertical Box to Panel..."
         panel.add(boxVertical)
         return panel
 
@@ -223,39 +235,40 @@ class BurpExtender(IBurpExtender, ITab):
             self._zonesLoaded.setText("")
             for i in range(len(zones)):
                 zone = zones[i]
-                domain = str(zone['domain'])
+                domain = str(zone["domain"])
                 print "Appending zone " + domain
                 self._zonesLoaded.append(domain)
                 if len(zones) > 1 and i != (len(zones) - 1):
                     self._zonesLoaded.append("\r\n")
             print "Zones loaded"
         else:
-            print 'Zones not loaded. No API Token or URL'
-        return ''
+            print "Zones not loaded. No API Token or URL"
+        return ""
 
     def getTabCaption(self):
-        return "BountyDNS"
+        return "Boucan"
 
     def getUiComponent(self):
         return self._panel
 
     def loadApiUrl(self):
-        return self._callbacks.loadExtensionSetting('api_url')
+        return self._callbacks.loadExtensionSetting("api_url")
 
     def saveApiUrl(self, val):
-        return self._callbacks.saveExtensionSetting('api_url', val)
+        return self._callbacks.saveExtensionSetting("api_url", val)
 
     def loadApiToken(self):
-        return self._callbacks.loadExtensionSetting('api_token')
+        return self._callbacks.loadExtensionSetting("api_token")
 
     def saveApiToken(self, val):
-        return self._callbacks.saveExtensionSetting('api_token', val)
+        return self._callbacks.saveExtensionSetting("api_token", val)
 
     def api(self):
         api_url = self._apiUrl.text
         api_token = self._apiToken.text
         print "Building API Client"
         return ApiClient(api_url, api_token)
+
 
 # Always at the end
 try:
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Burp Extension
 
 This folder containst the Burp Extension. The Burp Extension is written in Jython and is meant to inject payloads and communicate with the API
+
+More documentation can be found here: [Boucan: A Bug Bounty Canary Platform](https://github.com/3lpsy/boucanpy)
